f9d674258c282735c35a2729fe8dca71b09d483f
Stephen Thompson <s.thompson@ucl.ac.uk>
2024-09-16 21:32:16 +0100

    Added image resize

--- src/photoframe/image_process.py
+++ src/photoframe/image_process.py
@@ -21,7 +21,22 @@ def _resize_and_crop(image, frame_size = [1920, 1080],
             frame_size[1] - 2 * border_size[1]]
 
     print (f"Scale and crop to {target_size}")
-    
+
+    portrait = False
+    if height > width:
+        portrait = True
+
+    if portrait:
+        scale_factor = target_size[1]/height
+        scaled_image = cv2.resize(image, dsize=(0,0), fx = scale_factor, fy = scale_factor, 
+                interpolation = cv2.INTER_LANCZOS4)
+    else:
+        scale_factor = target_size[0]/width
+        scaled_image = cv2.resize(image, dsize=(0,0), fx = scale_factor, fy = scale_factor, 
+                interpolation = cv2.INTER_LANCZOS4)
+
+    print(f"Scaled to {scaled_image.shape}")
+
     #if it's landscape scale to fit width then crop top and bottom if required
     #if it's portrait scale to fit height, no need to crop.
 
