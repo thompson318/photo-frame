ff4ac19b436d11f3502caae1717f585009f89274
Stephen Thompson <s.thompson@ucl.ac.uk>
2024-10-10 12:01:07 +0100

    See if we can pass stuff to the webapp

--- app.py
+++ app.py
@@ -29,6 +29,13 @@ tasks = [
 def get_tasks():
    return jsonify({'tasks': tasks})
 
+@app.route('/photos', methods=['GET'])
+def list_photos():
+   return app.config['PHOTOS'].photos
+
+@app.route('/', methods=['GET'])
+def index():
+   return "photo-server"
 
 def record_loop(photolist, display):
    
@@ -43,11 +50,17 @@ def record_loop(photolist, display):
         display.show(image_to_display)
         time.sleep(1)
 
+def create_app(photo_instance, display_instance):
+    app = Flask(__name__)
+    app.config['PHOTOS'] = photo_instance
+    app.config['DISPLAY'] = display_instance
+    return app
 
 if __name__ == "__main__":
    photos = photolist()
    display = display(1.0)
    p = Process(target=record_loop, args=(photos,display))
    p.start()  
+   app = create_app(photos, display)
    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5001)
    p.join()
