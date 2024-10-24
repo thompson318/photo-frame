c98317a6104965f4f42c9e804939cc13742afb68
Stephen Thompson <s.thompson@ucl.ac.uk>
2024-10-24 08:21:46 +0100

    Added some init functions

--- src/photoframe/fb_display.py
+++ src/photoframe/fb_display.py
@@ -11,6 +11,7 @@ class fb_display():
         self.channels = 3
         self.fb = np.memmap('/dev/fb0', dtype='uint8',mode='w+', 
                 shape=(self.height, self.width, self.channels))
+        self._set_up_emojis():
 
     def show_photo(self, photo):
         # photo = cv2.imread('102D3500_orig_DSC_0317.JPG')
@@ -34,3 +35,28 @@ class fb_display():
     def show_crop(self):
         # an indicator to show that we've marked the image for crop
         self.fb[200:400, 300:500, :] = [0,255,0]
+
+    def show_patch(self, patch):
+        return
+
+    def _set_up_emojis(self):
+        try:
+            self.favourite = np.load('emojis/favourite.npy')
+        except FileNotFoundError:
+            self.favourite = np.full(
+                    shape=(200,200,4), fill_value=[255,0,0,255], dtype = np.uint8)
+
+        try:
+            self.crop_flag = np.load('crop_flag.npy')
+        except FileNotFoundError:
+            self.crop_flag = np.full(
+                    shape=(200,200,4), fill_value=[0,255,0,255], dtype = np.uint8)
+
+        try:
+            self.destroy = np.load('destroy.npy')
+        except FileNotFoundError:
+            self.destroy = np.full(
+                    shape=(200,200,4), fill_value=[0,0,0,255], dtype = np.uint8)
+
+
+
